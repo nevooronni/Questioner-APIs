338e60158886f6d01e68722c260f8deec44b37de
nevooronni <nevooronni@gmail.com>
2019-01-09 22:02:43 +0300

    [finishes #163071608] removed instance/config.py file from gitignore

---
+++ instance/config.py
@@ -0,0 +1,46 @@
+"""App configurations"""
+
+import os
+
+class Config(object):
+  """
+    Parent configuration class.
+  """
+
+  DEBUG = True
+  SECRET_KEY = os.getenv('SECRET_KEY')
+
+class DevelopmentConfig(Config):
+  """
+    Configurations for Development.
+  """
+  DEBUG = True
+
+class TestingConfig(Config):
+  """
+    Configuration for Testing with a seperate test database.
+  """
+
+  TESTING = True
+  DEBUG = True
+
+class StagingConfig(Config):
+  """
+    Configuration for Staging
+  """
+  DEBUG = True
+
+class ProductionConfig(Config):
+  """
+    Configuration for Production
+  """
+
+  DEBUG = False
+  TESTING = False
+
+app_config = {
+  'development': DevelopmentConfig,
+  'testing': TestingConfig,
+  'staging': StagingConfig,
+  'production': ProductionConfig,
+}
