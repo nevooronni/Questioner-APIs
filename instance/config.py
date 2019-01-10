1d762318c3a75c08162b5f65c04a8f13cc5465c1
nevooronni <nevooronni@gmail.com>
2019-01-10 20:18:32 +0300

    [finishes #163098161] removed jwt_secret_key

--- instance/config.py
+++ instance/config.py
@@ -7,8 +7,6 @@ class Config(object):
     Parent configuration class.
   """
 
-  JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
-  SECRET_KEY = os.getenv('SECRET_KEY')
   JWT_BLACKLIST_ENABLED = True
   JWT_BLACKLIST_TOKEN_CHECKS = ['access', 'refresh']
 
